e1439d0a126e55342381619c85eea9e626b37087
btroberts6566 <ben.roberts6566@gmail.com>
2024-06-01 13:45:37 +0100

    Home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,13 +12,13 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <div className='text-content'>
-        <h2>Bring your home to life.</h2>
-        <p>Elevate your living space with a touch of nature.We offer a range of easy-care houseplants.</p>
-        <button>Browse Plants</button>
+      <div className={styles.textContent}>
+        <h2 className={styles.subhead}>Bring your home to life.</h2>
+        <p className={styles.p1text}>Elevate your living space with a touch of nature. We offer a range of easy-care houseplants.</p>
+        <button className={styles.button}>Browse Plants</button>
       </div>
-      <div className='image-content'>
-        <img src={HomeImage} alt={"Living area with plants"} width={247} height={328.47}/>
+      <div>
+        <img className={styles.imageContent} src={HomeImage} alt={"Living area with plants"} width={247} height={328.47}/>
       </div>
     </div>  
   );
