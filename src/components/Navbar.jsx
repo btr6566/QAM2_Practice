42742d90d3a80f2b59cd68ecfe1dcd35f64fd5ab
btroberts6566 <ben.roberts6566@gmail.com>
2024-06-01 12:53:09 +0100

    Navbar CSS Seperated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import '../style.css'
+// import '../style.css'
+import '../styles/Navbar.css'
 
 export default function Navbar() {
 
